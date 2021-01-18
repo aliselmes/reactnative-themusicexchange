fbe22d5cb5bcdc9d80ab6ee8ebe039e77a0f4383
aliselmes <a.selmes@gmail.com>
2021-01-18 16:17:04 -0600

    added swipe to  delete functionality to saved items

--- components/FavoritesComponent.js
+++ components/FavoritesComponent.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
-import { FlatList, View, Text } from 'react-native';
+import { FlatList, View, Text, StyleSheet, Alert } from 'react-native';
 import { ListItem } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { Loading } from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
+import { SwipeRow } from 'react-native-swipe-list-view';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import { deleteFavorite } from '../redux/ActionCreators';
 
 const mapStateToProps = state => {
     return {
@@ -12,21 +15,56 @@ const mapStateToProps = state => {
     };
 };
 
+const mapDispatchToProps = {
+    deleteFavorite: itemId => deleteFavorite(itemId)
+};
+
 class Favorites extends Component {
 
     static navigationOptions = {
-        title: 'My Favorites'
+        title: 'My Saved Items' 
     }
 
     render() {
         const { navigate } = this.props.navigation;
         const renderFavoriteItem = ({item}) => {
             return (
-                <ListItem
-                    title={item.name}
-                    leftAvatar={{source: {uri: baseUrl + item.image}}}
-                    onPress={() => navigate('GearInfo', {itemId: item.id})} 
-                />
+                <SwipeRow rightOpenValue={-100} style={styles.swipeRow}>
+                    <View style={styles.deleteView}>
+                        <TouchableOpacity
+                            style={styles.deleteTouchable}
+                            onPress={() =>
+                                Alert.alert(
+                                    'Delete Favorite?',
+                                    'Are you sure you wish to delete the saved item: ' +
+                                        item.name +
+                                        '?',
+                                    [
+                                        {
+                                            text: 'Cancel',
+                                            onPress: () => console.log(item.name + 'Not Deleted'),
+                                            style: 'cancel'
+                                        },
+                                        {
+                                            text: 'OK',
+                                            onPress: () => this.props.deleteFavorite(item.id)
+                                        },
+                                    ],
+                                    { cancelable: false }
+                                )
+                            }
+                        >
+                            <Text style={styles.deleteText}>Delete</Text> 
+                        </TouchableOpacity>
+                    </View>
+                    <View>
+                        <ListItem
+                            title={item.name}
+                            leftAvatar={{source: {uri: baseUrl + item.image}}}
+                            onPress={() => navigate('GearInfo', {itemId: item.id})} 
+                        />
+                    </View>
+                </SwipeRow>
             );
         };
 
@@ -52,4 +90,25 @@ class Favorites extends Component {
     }
 }
 
-export default connect(mapStateToProps)(Favorites);
+const styles = StyleSheet.create({
+    deleteView: {
+        flexDirection: 'row',
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+        flex: 1
+    },
+    deleteTouchable: {
+        backgroundColor: 'red',
+        height: '100%',
+        justifyContent: 'center'
+    },
+    deleteText: {
+        color: 'white',
+        fontWeight: '700',
+        textAlign: 'center',
+        fontSize: 16,
+        width: 100
+    }
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Favorites);
--- redux/ActionCreators.js
+++ redux/ActionCreators.js
@@ -159,3 +159,8 @@ export const addFavorite = itemId => ({
     type: ActionTypes.ADD_FAVORITE,
     payload: itemId
 });
+
+export const deleteFavorite = itemId => ({
+    type: ActionTypes.DELETE_FAVORITE,
+    payload: itemId
+});
